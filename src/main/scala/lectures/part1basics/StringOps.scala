e6925e23ad2a8d109235922d17a515fd456dc1c4
Sherif Tarek <sherif.tarek.software.engineer@gmail.com>
2024-02-08 11:59:54 +0200

    Basics 2.9: String Ops

---
+++ src/main/scala/lectures/part1basics/StringOps.scala
@@ -0,0 +1,25 @@
+package lectures.part1basics
+
+object StringOps extends App {
+  val str: String = "Hello, I am learning Scala"
+  println(str.replace(' ', '-'))
+  println(str.replaceAll("[a-zA-Z]", "-"))
+
+  val x = "2"
+  val aStr: String = x.+:('a') :+ 'z'
+  val aVector: Vector[Matchable] = ("a" +: x :+ "z").toVector
+  println('a' +: x :+ 'z')
+  println('a' +: x :+ "z")
+  val name = "Sherif"
+  val age = 25
+  val sGreeting = s"This is $name, \n and I'm $age years old"
+  println(sGreeting)
+
+  val fGreeting = f"This is $name, \n and I'm $age years old"
+  println(fGreeting)
+
+  val rawGreeting: String = raw"This is $name, \n and I'm $age years old"
+  println(rawGreeting)
+
+
+}
