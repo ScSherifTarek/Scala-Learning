65379fd7363402fb68e8fd75ba8caea07203ab33
Sherif Tarek <sherif.tarek.software.engineer@gmail.com>
2024-02-11 11:18:46 +0200

    Basics 3.14: Scala Objects

--- src/main/scala/lectures/part2oop/Objects.scala
+++ src/main/scala/lectures/part2oop/Objects.scala
@@ -8,14 +8,18 @@ object Objects extends App {
   object Person {
     val N = 10
     def apply(name: String) = new Person(name)
+    def whisper(person: Person): Unit = person privatePrint
   }
   class Person(val name: String) {
     def print: Unit = println(s"Hello I'm $name")
+    private def privatePrint: Unit = println(s"Psst, Hi, I'm $name")
   }
 
   val p1 = new Person("Sherif")
   p1 print
 
-  val p2 = new Person("Sherif")
+  val p2 = new Person("Ahmed")
   p2 print
+
+  Person.whisper(p2)
 }
