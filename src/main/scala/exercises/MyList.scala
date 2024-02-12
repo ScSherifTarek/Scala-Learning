d274047e5059e28ae5509fd73c2747e6599e5650
Sherif Tarek <sherif.tarek.software.engineer@gmail.com>
2024-02-12 10:56:34 +0200

    Revert "Basics 3.17: MyList immutable in-order implementation"
    This reverts commit 1354532d9984868a5518db8236d2bd6f023be8a8.

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -1,42 +1,33 @@
 package exercises
 
-
-class Node(val value: Int)
-
 abstract class MyList {
-  def getHead: Node
-  def getTail: Node
+  def getHead: Int
+  def getTail: MyList
   def isEmpty: Boolean
   def add(value: Int): MyList
   def toString: String
 }
 
-class EmptyList extends MyList {
-  def getHead: Node = null
-  def getTail: Node = null
+object EmptyList extends MyList {
+  def getHead: Int = throw new NoSuchElementException
+  def getTail: MyList = throw new NoSuchElementException
   def isEmpty: Boolean = true
   def add(value: Int): MyList = new List(value, this)
   override def toString: String = ""
 }
 
-class List(val value: Int, prev: MyList) extends MyList {
-  val tail = new Node(value)
-
-  def getHead: Node = {
-    val prevHead: Node = prev.getHead
-    if (prevHead == null) tail
-    else prevHead
-  }
-  def getTail: Node = tail
+class List(val head: Int, tail: MyList) extends MyList {
+  def getHead: Int = head
+  def getTail: MyList = tail
   def isEmpty: Boolean = false
   def add(value: Int): MyList = new List(value, this)
   override def toString: String =
-    if (prev.isEmpty) s"${tail.value}"
-    else s"${prev.toString} ${tail.value}"
+    if(tail.isEmpty) s"$head"
+    else s"$head ${tail.toString}"
 }
 
 object MyList {
-  def apply(): MyList =  new EmptyList
+  def apply(): MyList =  EmptyList
 }
 
 object TestMyList extends App {
@@ -44,8 +35,8 @@ object TestMyList extends App {
 
   val updatedList = list.add(5).add(6)
   println(updatedList.isEmpty)
-  println(updatedList.getHead.value)
-  println(updatedList.getTail.value)
+  println(updatedList.getHead)
+  println(updatedList.getTail)
   println(updatedList.toString)
 }
 
