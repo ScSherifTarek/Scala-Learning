eb1f956c13458219a2e7959fa5df571f4821e3c8
Sherif Tarek <sherif.tarek.software.engineer@gmail.com>
2024-02-12 12:13:39 +0200

    Basics 3.18: Generics

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -1,39 +1,40 @@
 package exercises
 
-abstract class MyList {
-  def getHead: Int
-  def getTail: MyList
+abstract class MyList[+A] {
+  def getHead: A
+  def getTail: MyList[A]
   def isEmpty: Boolean
-  def add(value: Int): MyList
+  def add[B >: A](value: B): MyList[B]
   def toString: String
 }
 
-object EmptyList extends MyList {
-  def getHead: Int = throw new NoSuchElementException
-  def getTail: MyList = throw new NoSuchElementException
+object EmptyList extends MyList[Nothing] {
+  def getHead: Nothing = throw new NoSuchElementException
+  def getTail: MyList[Nothing] = throw new NoSuchElementException
   def isEmpty: Boolean = true
-  def add(value: Int): MyList = new List(value, this)
+  def add[A >: Nothing](value: A): MyList[A] = new List[A](value, this)
   override def toString: String = ""
 }
 
-class List(val head: Int, tail: MyList) extends MyList {
-  def getHead: Int = head
-  def getTail: MyList = tail
+class List[+A](val head: A, tail: MyList[A]) extends MyList[A] {
+  def getHead: A = head
+  def getTail: MyList[A] = tail
   def isEmpty: Boolean = false
-  def add(value: Int): MyList = new List(value, this)
+  def add[B >: A](value: B): MyList[B] = new List[B](value, this)
   override def toString: String =
     if(tail.isEmpty) s"$head"
     else s"$head ${tail.toString}"
 }
 
 object MyList {
-  def apply(): MyList =  EmptyList
+  def apply[A](): MyList[A] =  EmptyList
 }
 
 object TestMyList extends App {
-  val list: MyList = MyList()
+  val list: MyList[Any] = MyList[Int]()
+  println(list.isEmpty)
 
-  val updatedList = list.add(5).add(6)
+  val updatedList = list.add(5).add(6).add("Hamada").add(4.0)
   println(updatedList.isEmpty)
   println(updatedList.getHead)
   println(updatedList.getTail)
---
+++ src/main/scala/lectures/part2oop/Generics.scala
@@ -0,0 +1,26 @@
+package lectures.part2oop
+
+object Generics extends App {
+  class Animal
+  class Cat extends Animal
+  class Dog extends Animal
+
+  // Variance Problem
+  // If Cat extends Animal, would List[Animal] = List[Cat] work?
+
+  // 1. Yes, Covariance
+  class CovariantList[+A] {
+    def add[B >: A](animal: B): ContravariantList[B] = new ContravariantList[B]
+  }
+  val covariantList: CovariantList[Animal] = new CovariantList[Cat]
+  val newList = covariantList.add(new Dog) // -> returns a list of animal
+
+  // 2. No, Invariance -- only the type defined at the beginning
+  class InvariantList[A]
+  val invariantList: InvariantList[Animal] = new InvariantList[Animal]
+
+  // 3. Hell, no! Contravariance -- only a super class is allowed (List[Cat] = List[Animal])
+  class ContravariantList[-A]
+  val contravariantList: ContravariantList[Cat] = new ContravariantList[Animal]
+
+}
