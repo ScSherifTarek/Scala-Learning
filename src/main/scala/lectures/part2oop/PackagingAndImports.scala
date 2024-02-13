490c4003524f920836130f3e155bf7b1b0b36f2a
Sherif Tarek <sherif.tarek.software.engineer@gmail.com>
2024-02-13 10:15:16 +0200

    Basics 3.24: Packaging and Imports

---
+++ src/main/scala/lectures/part2oop/PackagingAndImports.scala
@@ -0,0 +1,6 @@
+package lectures.part2oop
+
+object PackagingAndImports extends App {
+  sayHello()
+  println(SPEED_OF_LIGHT)
+}
---
+++ src/main/scala/lectures/part2oop/package.scala
@@ -0,0 +1,6 @@
+package lectures
+
+package object part2oop {
+  def sayHello(): Unit = println("Hello from part2oop package object")
+  val SPEED_OF_LIGHT: Int = 299792458
+}
