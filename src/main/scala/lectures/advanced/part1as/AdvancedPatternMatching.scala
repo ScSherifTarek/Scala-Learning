00ca38b1e7bc9dbfb49fb781a7dca174e03c3ec5
Sherif Tarek <sherif.tarek.software.engineer@gmail.com>
2024-02-21 09:30:42 +0300

    Advanced 2.6: Advanced Pattern Matching - Updated Solution

--- src/main/scala/lectures/advanced/part1as/AdvancedPatternMatching.scala
+++ src/main/scala/lectures/advanced/part1as/AdvancedPatternMatching.scala
@@ -3,18 +3,18 @@ package lectures.advanced.part1as
 object AdvancedPatternMatching extends App {
 
   object Even {
-    def unapply(arg: Int): Option[Int] = if arg % 2 == 0 then Some(arg) else None
+    def unapply(arg: Int): Boolean = arg % 2 == 0
   }
 
   object OneDigit {
-    def unapply(arg: Int): Option[Int] = if arg < 10 then Some(arg) else None
+    def unapply(arg: Int): Boolean = arg < 10
   }
 
   val n: Int = 5
   val matchPattern = n match {
-    case Even(n) => s"${n} is an even number"
-    case OneDigit(n) => s"${n} is a one digit number"
-    case n => s"${n} has no property"
+    case Even() => s"${n} is an even number"
+    case OneDigit() => s"${n} is a one digit number"
+    case _ => s"${n} has no property"
   }
   println(matchPattern)
 }
