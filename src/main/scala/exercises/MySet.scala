2c36a767fe9e4bb4e28ae8bc472a2391d69fb321
Sherif Tarek <sherif.tarek.software.engineer@gmail.com>
2024-02-25 19:27:31 +0300

    Advanced 3.12: Functional Set, level 9000

--- src/main/scala/exercises/MySet.scala
+++ src/main/scala/exercises/MySet.scala
@@ -13,8 +13,9 @@ trait MySet[A] extends (A => Boolean) {
   def isEmpty: Boolean = true
   def getPrintFormat: String
   def -(v: A): MySet[A] = this.filter(_ != v)
-  def &(another: MySet[A]): MySet[A] = this.filter(another.contains)
-  def --(another: MySet[A]): MySet[A] = this.filter(!another.contains(_))
+  def &(another: MySet[A]): MySet[A] = this.filter(another)
+  def --(another: MySet[A]): MySet[A] = this.filter(!another)
+  def unary_! : MySet[A]
   override def apply(v1: A): Boolean = this.contains(v1)
   override def toString(): String = "(" + this.getPrintFormat + ")"
 }
@@ -28,6 +29,7 @@ class EmptySet[A] extends MySet[A] {
   override def flatMap[B](f: A => MySet[B]): MySet[B] = new EmptySet[B]
   override def foreach(f: A => Unit): Unit = ()
   override def getPrintFormat: String = ""
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](_ => true)
 }
 
 case class NonEmptySet[A](val head: A, val rest: MySet[A]) extends MySet[A] {
@@ -55,6 +57,37 @@ case class NonEmptySet[A](val head: A, val rest: MySet[A]) extends MySet[A] {
   override def getPrintFormat: String =
     if(rest.isEmpty) s"${head}"
     else s"${head} ${rest.getPrintFormat}"
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](!contains(_))
+}
+
+class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
+  def contains(elem: A): Boolean = property(elem)
+  def +(elem: A): MySet[A] = {
+    if(contains(elem)) this
+    else new PropertyBasedSet[A](x => property(x) || x == elem)
+  }
+  def ++(another: MySet[A]): MySet[A] = new PropertyBasedSet[A](x => property(x) || another(x))
+
+  def map[B](f: A => B): MySet[B] = politelyFail
+
+  def flatMap[B](f: A => MySet[B]): MySet[B] = politelyFail
+
+  def filter(predicate: A => Boolean): MySet[A] = new PropertyBasedSet[A](x => property(x) && predicate(x))
+
+  def foreach(f: A => Unit): Unit = politelyFail
+
+  override def isEmpty: Boolean = false
+
+  def getPrintFormat: String = "Property based set"
+
+  override def -(v: A): MySet[A] = filter(_ != v)
+
+  override def &(another: MySet[A]): MySet[A] = this.filter(another)
+
+  override def --(another: MySet[A]): MySet[A] = this.filter(!another)
+
+  def unary_! : MySet[A] = new PropertyBasedSet[A](x => !property(x))
+  def politelyFail = throw new IllegalArgumentException("Really deep rabbit hole!")
 }
 
 object MySet {
@@ -86,5 +119,34 @@ object TestMySet extends App {
   println(s1 - 5)
   println(s1 & s2)
   println(s1 -- s2)
+
+  val s1Negated = !s1
+  println(s1Negated.contains(5))
+  println(s1Negated.contains(1))
+  println(s1Negated.filter(_ % 2 == 0).contains(1))
+  println(s1Negated.filter(_ % 2 == 0).contains(4))
+  println(s1Negated.filter(_ % 2 == 0) + 1 contains 1)
+  println(s1Negated ++ s2 contains 5)
+  println(s1Negated ++ s2 - 5 contains 5)
+  println(s1Negated & MySet(2) contains 4)
+  println(s1Negated & MySet(2) contains 2)
+  println(s1Negated -- MySet(2) contains 4)
+  println(s1Negated -- MySet(2) contains 2)
+
+  println("Course test cases:")
+  val s = MySet(1, 2, 3, 4)
+  val x = s + 5 ++ MySet(-1, -2) + 3 flatMap (x => MySet(x, 10 * x)) filter (_ % 2 == 0)
+  println(x)
+
+  println(s)
+  val negative = !s // s.unary_! = all the naturals not equal to 1,2,3,4
+  println(negative(2))
+  println(negative(5))
+
+  val negativeEven = negative.filter(_ % 2 == 0)
+  println(negativeEven(5))
+
+  val negativeEven5 = negativeEven + 5 // all the even numbers > 4 + 5
+  println(negativeEven5(5))
 }
 
