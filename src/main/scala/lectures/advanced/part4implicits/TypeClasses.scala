2b81b9444e0876af58ef8da22fa45a6df2b0478e
Sherif Tarek <sherif.tarek.software.engineer@gmail.com>
2024-03-04 14:49:11 +0300

    Advanced 5.35: Type Classes

---
+++ src/main/scala/lectures/advanced/part4implicits/TypeClasses.scala
@@ -0,0 +1,25 @@
+package lectures.advanced.part4implicits
+
+object TypeClasses extends App {
+  trait Equal[T] {
+    def apply(a: T, b: T): Boolean
+  }
+
+  case class Person(name: String, age: Int, email: String)
+  implicit object NameEqual extends Equal[Person] {
+    override def apply(a: Person, b: Person): Boolean = a.name == b.name
+  }
+
+  object FullEqual extends Equal[Person] {
+    override def apply(a: Person, b: Person): Boolean = a.name == b.name && a.age == b.age && a.email == b.email
+  }
+
+  object Equal {
+    def apply[T](a: T, b: T)(implicit equalizer: Equal[T]): Boolean = equalizer.apply(a, b)
+  }
+
+  val sherif = Person("Sherif", 25, "sherif@backend.com")
+  val sherif2 = Person("Sherif", 32, "ahmed@backend.com")
+  println(Equal(sherif, sherif2))
+  println(Equal(sherif, sherif2)(FullEqual))
+}
